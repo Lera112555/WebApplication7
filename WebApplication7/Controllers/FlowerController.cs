18a843eaa45086b61fbbec5dfd1057cb23374f1a
Lera112555 <73837980+Lera112555@users.noreply.github.com>
2021-10-25 15:07:48 +1000

    Добавьте файлы проекта.

---
+++ WebApplication7/Controllers/FlowerController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication7.Data.IntFc;
+
+namespace WebApplication7.Controllers
+{
+    public class FlowerController : Controller 
+    {
+        private readonly IAllFlowers _allFlowers;
+        private readonly IFlowerCategory _allCategories; 
+        
+        public FlowerController(IAllFlowers iAllFlowers, IFlowerCategory iFlowerCat)
+        {
+            _allFlowers = iAllFlowers;
+            _allCategories = iFlowerCat; 
+        }
+
+        public ViewResult List()
+        {
+            var flowers = _allFlowers.Flowers; 
+            return View(); 
+        }
+    }
+}
---
+++ WebApplication7/Data/mocks/MockFlower.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication7.Data.IntFc;
+using WebApplication7.Data.Models;
+
+namespace WebApplication7.Data.mocks
+{
+    public class MockFlower : IAllFlowers
+    {
+        private readonly IFlowerCategory _categoryFlowers = new MockCategory(); 
+        public IEnumerable<Flower> Flowers
+        {
+            get
+            {
+                return new List<Flower>
+                {
+                    new Flower {name = "Эустома", shortDesk="", longDesc="", img="", price=500, isFavorite=true, availble=true, Category=_categoryFlowers.AllCategories.First()}
+                };
+            }
+        } 
+        public IEnumerable<Flower> getFavFlowers { get; set; }
+
+        public Flower getObjectCar(int flowerId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
